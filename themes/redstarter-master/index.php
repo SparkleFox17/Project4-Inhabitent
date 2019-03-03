b5cbcb020fc87a22c949d03d8e476d380ffc5e39
stephanie133.tait@gmail.com <stephanie133.tait@gmail.com>
2019-03-03 01:44:02 -0500

    added mapped styles

--- themes/redstarter-master/index.php
+++ themes/redstarter-master/index.php
@@ -10,6 +10,36 @@ get_header(); ?>
 	<div id="primary" class="content-area home">
 		<main id="main" class="site-main" role="main">
 
+		<section class="home-hero">
+            <img src="http://localhost:8888/wordpress/wp-content/themes/redstarter-master/images/inhabitent-logo-full.svg" class="logo" alt="Inhabitent full logo">
+		 </section>
+		 
+		 <section class="product-info container">
+            <h2>Shop Stuff</h2>
+			<div class="product-type-blocks">
+				<div class="product-type-block-wrapper">
+					<img src="http://localhost:8888/wordpress/wp-content/themes/redstarter-master/images/do.svg" alt="Do">
+					<p>Get back to nature with all the tools and toys you need to enjoy the great outdoors.</p>
+					<p><a href="http://localhost:8888/wordpress/product-type/do/" class="btn">Do Stuff</a></p>
+				</div>
+				<div class="product-type-block-wrapper">
+					<img src="http://localhost:8888/wordpress/wp-content/themes/redstarter-master/images/eat.svg" alt="Eat">
+					<p>Nothing beats food cooked over a fire. We have all you need for good camping eats.</p>
+					<p><a href="http://localhost:8888/wordpress/product-type/eat/" class="btn">Eat Stuff</a></p>
+				</div>
+				<div class="product-type-block-wrapper">
+					<img src="http://localhost:8888/wordpress/wp-content/themes/redstarter-master/images/sleep.svg" alt="Sleep">
+					<p>Get a good night's rest in the wild in a home away from home that travels well.</p>
+					<p><a href="http://localhost:8888/wordpress/product-type/sleep/" class="btn">Sleep Stuff</a></p>
+				</div>
+				<div class="product-type-block-wrapper">
+					<img src="http://localhost:8888/wordpress/wp-content/themes/redstarter-master/images/wear.svg" alt="Wear">
+					<p>From flannel shirts to toques, look the part while roughing it in the great outdoors.</p>
+					<p><a href="http://localhost:8888/wordpress/product-type/wear/" class="btn">Wear Stuff</a></p>
+				</div>
+			</div>
+		</section>
+
 		<?php if ( have_posts() ) : ?>
 
 			<?php if ( is_home() && ! is_front_page() ) : ?>
